2cb24c4f9e4c93115f8339665823151bf11d01ad
yasirnurprasetyo <yasirnurprasetyo@gmail.com>
2021-09-24 20:43:54 +0700

    fix app 1.2

---
+++ app/src/main/java/com/yasir/tinalahedu/ui/activity/highScore/VerifikasiHighScoreActivity.kt
@@ -0,0 +1,11 @@
+package com.yasir.tinalahedu.ui.activity.highScore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VerifikasiHighScoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verifikasi_high_score)
+    }
+}
